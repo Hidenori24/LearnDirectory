0b4a62fbdc8fc5ce86fe2fe1b7d5c1ee94341cad
matsumoto <matsumoto@matsumoto.local>
2019-04-02 01:43:13 +0900

    Non improvement Learn

--- simpleModel_Or.py
+++ simpleModel_Or.py
@@ -4,37 +4,32 @@
 import numpy as np
 import chainer.functions as F
 import chainer.links as L
-from chainer import Variable, optimizers
+from chainer import Variable, optimizers,Chain
 
-#モデル定義
-model = L.Linear(1,1)
-optimizer = optimizers.SGD()
+#モデルクラス定義
+class Model(Chain):
+    def __init__(self):
+        super(Model, self).__init__(
+            l1 = L.Linear(2,1),
+            )
+    def __call__(self,x):
+        h = self.l1(x)
+        return h
+
+model = Model()
+optimizer = optimizers.MomentumSGD(lr=0.01, momentum=0.9)
 optimizer.setup(model)
 
-# 学習回数
-times = 50
-# 入力ベクトル
-x = Variable(np.array([[1]], dtype=np.float32))
+x = Variable(np.array([[0,0],[0,1],[1,0],[1,1]], dtype=np.float32))
+t = Variable(np.array([[0],[1],[1],[1]], dtype=np.float32))
 
-# 正解ベクトル
-t = Variable(np.array([[2]], dtype=np.float32))
-
-# 学習ループ
-for i in range(0,times):
-    # 勾配初期化
+for i in range(0,3000):
     optimizer.zero_grads()
-
-    # モデル予測
     y = model(x)
-
-    # モデルが出した答えの表示
-    print(y.data)
-
-    # 損失計算
     loss = F.mean_squared_error(y,t)
-
-    # 逆伝播
     loss.backward()
-
-    # optimizerを更新する
     optimizer.update()
+
+    print("loss:" ,loss.data)
+
+print(y.data)
